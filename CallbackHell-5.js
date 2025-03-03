83f4cd17e58b516488224abe381e06bf899f0c05
Mrinal47 <Mrinalanand47@gmail.com>
2025-03-03 21:00:50 +0530

    Callback Hell - 5 Task Done

---
+++ CallbackHell-5.js
@@ -0,0 +1,33 @@
+function add(value, callback) {
+    // complete this function
+  
+    setTimeout(() => {
+      value = value + 3;
+      console.log("Added 3");
+      callback(value);
+    }, 2000);
+  
+  }
+  
+  
+  function multiply(value, callback) {
+     // complete this function
+  
+    setTimeout(() => {
+      value = value * 2;
+      console.log("Multiplied by 2");
+      callback(value);
+    }, 1000);
+  }
+   
+  
+  // don't change it
+  const initialValue = 2;
+  
+  
+  // create callback hell 
+  add(initialValue, (newValue) => {
+    multiply(newValue, (finalValue) => {
+      console.log("Final Result:", finalValue);
+    });
+  });
