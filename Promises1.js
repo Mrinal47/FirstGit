8e7ed366128a876f56e7a1e091a87a1fb1b4857f
Mrinal47 <Mrinalanand47@gmail.com>
2025-03-04 13:21:54 +0530

    Promises - 1 Done

---
+++ Promises1.js
@@ -0,0 +1,20 @@
+function bookSearchPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            const bookSearch = Math.random() > 0.5;
+            if(bookSearch) {
+                resolve("Book found");
+            } else {
+                reject("Book not found");
+            }
+        }, 3000);
+    });
+}
+
+bookSearchPromise()
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
